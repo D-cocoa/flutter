5672b28884da2effab34ffe9252b09decc9d97c2
yueC <865654169@qq.com>
2021-09-10 17:23:47 +0800

    9.10

---
+++ note/lib/ViewModel/login_vm.dart
@@ -0,0 +1,13 @@
+
+void login (String username,String password)async{
+ /*
+                if (username=='110' && password=='6155') {
+                  Navigator.popAndPushNamed(context, 'home');
+                }else{
+                   //  _showLoginError();
+                   _showLoginAlert();
+                }
+                
+
+    */        
+}
